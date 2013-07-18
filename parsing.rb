d8883b1142104df4c4048f03900aab0a53fd0581
Anthony Madhvani <anny.mdvn@gmail.com>
2013-07-19 01:25:04 +0200

    Mimic Reddit's user URLs

--- parsing.rb
+++ parsing.rb
@@ -3,6 +3,21 @@ require 'sinatra'
 require "json"
 require "ap"
 
+#Set global variables
+configure do
+  Sort = OpenStruct.new(
+    :hot => 'hot',
+    :newsort => 'new',
+    :top => 'top',
+    :controversial => 'controversial'
+  )
+  Type = OpenStruct.new(
+    :submitted => 'submitted',
+    :comments => 'comments',
+    :overview => 'overview'
+  )
+end
+
 get '/' do
 	erb :post
 end
@@ -16,7 +31,38 @@ get '/:user' do
 end
 
 post '/' do
-	user = Redditor.new(params[:user],params[:sort],params[:limit],params[:type])
+	sort = "sort="
+	sort << params[:sort]
+	limit = "limit="
+	limit << params[:limit]
+
+	redirect to("/user/#{params[:user]}/#{params[:type]}/?#{sort}&#{limit}")
+end
+
+get '/test/:user/:type/' do
+	sort = "?sort="
+	sort << params[:sort]
+	redirect to("/user/#{params[:user]}/#{params[:type]}/#{sort}")
+end
+
+get '/user/:user' do
+	redirect to("/user/#{params[:user]}/overview/?sort=new")
+end
+
+#This route is analogous to how Reddit routes pages
+get '/user/:user/:type/' do
+	#Set initial values, override if any custom ones are given
+	limit = 100
+	if params[:limit]
+		limit = params[:limit]
+	end
+
+	sort = "new"
+	if params[:sort]
+		sort = params[:sort]
+	end
+
+	user = Redditor.new(params[:user],params[:type],params[:sort],limit)
 
 	@ups = user.results[0]
 	@downs = user.results[1]
@@ -37,11 +83,16 @@ class Redditor
 	@@user_agent = "Redditalytics alpha [/u/pegasus_527, github/octagonal/redditalytics]"
 	attr_reader :user, :sort, :limit, :type, :results
 
-	def initialize(user,sort,limit,type)
+	# sort 	=> hot, new, top, controversial
+	# limit => 0..100
+	# type 	=> submitted, comments, overview (all
+	# user 	=> String
+	def initialize(user,type,sort,limit=100)
 		@user = user
 		@sort = sort
 		@limit = limit
 		@type = type
+		puts "#{user}#{sort}#{limit}#{type}"
 	end
 
 	def results
@@ -59,9 +110,9 @@ class Redditor
 			data_node = []
 			data_hash = child["data"]
 			case @type
-			when "comment"
+			when "comments"
 				data_node << data_hash["body"]
-			when "link"
+			when "submitted"
 				data_node << data_hash["title"]
 			when "overview"
 				if data_hash["title"] == nil
@@ -84,6 +135,8 @@ class Redditor
 			data_node = []
 		end
 
+		ap acc_up
+
 		#Make it chronological
 		acc_up.reverse!
 		acc_downs.reverse!
@@ -104,9 +157,9 @@ class Redditor
 		formed_url << "/user/#{@user}/"
 
 		case @type
-		when "comment"
+		when "comments"
 			formed_url << "comments"
-		when "link"
+		when "submitted"
 			formed_url << "submitted"
 		when "overview"
 			formed_url << "overview"
